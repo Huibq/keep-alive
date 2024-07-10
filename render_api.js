f029fc79f7ee9c259aef43fa68554879dd63fb14
Huibq <huibq520@gmail.com>
2024-07-10 13:52:38 +0800

    render源

---
+++ render_api.js
@@ -0,0 +1,89 @@
+/*!
+ * @name Huibq <huibq520@gmail.com>_render源
+ * @description 为应对vercel资源定价改变，使用render部署
+ * @version v1.2.0
+ * @author Huibq <huibq520@gmail.com>
+ */
+const DEV_ENABLE = false
+const API_URL = 'https://render.niuma666bet.buzz'
+const API_KEY = 'share-v2'
+const MUSIC_QUALITY = {
+  kw: ['128k', '320k'],
+  kg: ['128k', '320k'],
+  tx: ['128k', '320k'],
+  wy: ['128k', '320k'],
+  mg: ['128k', '320k'],
+}
+const MUSIC_SOURCE = Object.keys(MUSIC_QUALITY)
+const { EVENT_NAMES, request, on, send, utils, env, version } = globalThis.lx
+const httpFetch = (url, options = { method: 'GET' }) => {
+  return new Promise((resolve, reject) => {
+    request(url, options, (err, resp) => {
+      if (err) return reject(err)
+      resolve(resp)
+    })
+  })
+}
+const handleGetMusicUrl = async (source, musicInfo, quality) => {
+  const songId = musicInfo.hash ?? musicInfo.songmid
+
+  const request = await httpFetch(`${API_URL}/url/${source}/${songId}/${quality}`, {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json',
+      'User-Agent': `${env ? `lx-music-${env}/${version}` : `lx-usic-request/${version}`}`,
+      'X-Request-Key': API_KEY,
+    },
+  })
+  const { body } = request
+  if (!body || isNaN(Number(body.code))) throw new Error('unknow error')
+  switch (body.code) {
+    case 0:
+      return body.url
+    case 1:
+      throw new Error('block ip')
+    case 2:
+      throw new Error('get music url failed')
+    case 4:
+      throw new Error('internal server error')
+    case 5:
+      throw new Error('too many requests')
+    case 6:
+      throw new Error('param error')
+    default:
+      throw new Error(body.msg ?? 'unknow error')
+  }
+}
+const musicSources = {}
+MUSIC_SOURCE.forEach(item => {
+  musicSources[item] = {
+    name: item,
+    type: 'music',
+    actions: ['musicUrl'],
+    qualitys: MUSIC_QUALITY[item],
+  }
+})
+on(EVENT_NAMES.request, ({ action, source, info }) => {
+  switch (action) {
+    case 'musicUrl':
+      if (env != 'mobile') {
+        console.group(`Handle Action(musicUrl)`)
+        console.log('source', source)
+        console.log('quality', info.type)
+        console.log('musicInfo', info.musicInfo)
+        console.groupEnd()
+      } else {
+        console.log(`Handle Action(musicUrl)`)
+        console.log('source', source)
+        console.log('quality', info.type)
+        console.log('musicInfo', info.musicInfo)
+      }
+      return handleGetMusicUrl(source, info.musicInfo, info.type)
+        .then(data => Promise.resolve(data))
+        .catch(err => Promise.reject(err))
+    default:
+      console.error(`action(${action}) not support`)
+      return Promise.reject('action not support')
+  }
+})
+send(EVENT_NAMES.inited, { status: true, openDevTools: DEV_ENABLE, sources: musicSources })
